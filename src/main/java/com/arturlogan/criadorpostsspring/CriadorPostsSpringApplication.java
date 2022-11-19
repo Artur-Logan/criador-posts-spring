d6554ec9fefd79978bb93cedb6f4960ba5f121e2
Artur Logan <arturlogan1ferreira@gmail.com>
2022-11-18 21:43:42 -0300

    Primeiro commit.

---
+++ src/main/java/com/arturlogan/criadorpostsspring/CriadorPostsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.criadorpostsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriadorPostsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriadorPostsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturlogan/criadorpostsspring/CriadorPostsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.criadorpostsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriadorPostsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
