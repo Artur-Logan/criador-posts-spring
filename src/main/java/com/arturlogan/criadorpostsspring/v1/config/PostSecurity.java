d314b1826ee12b07ee214844dd44118021586e7a
Artur Logan <arturlogan1ferreira@gmail.com>
2022-12-01 21:45:58 -0300

    Implementação do Oauth2 e suas configurações

---
+++ src/main/java/com/arturlogan/criadorpostsspring/v1/config/PostSecurity.java
@@ -0,0 +1,33 @@
+package com.arturlogan.criadorpostsspring.v1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class PostSecurity extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder.inMemoryAuthentication().withUser("teste").password("teste123").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
